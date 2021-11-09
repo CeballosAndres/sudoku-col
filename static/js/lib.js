c17509ef83dca9bb38d0fb3419a9f8fed0dacab5
AleCentalz <palkya58@gmail.com>
2021-11-09 13:21:57 -0600

    Restric inputs

--- static/js/lib.js
+++ static/js/lib.js
@@ -1,3 +1,12 @@
+function onlyNumberKey(evt) {    
+  // Only ASCII character in that range allowed
+  var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+  if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+      return false;
+  return true;
+}
+
+
 function numbers(){
   let numbers_matrix = '';
   for(let i = 0; i <= 80; i++){
