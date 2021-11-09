6a455df19cb56cce76b5f8fad839984070fa9a3f
AleCentalz <palkya58@gmail.com>
2021-11-09 13:26:46 -0600

    Restric the 0

--- static/js/lib.js
+++ static/js/lib.js
@@ -1,11 +1,10 @@
+//Funcion para restringir la entrada de los input de 1-9 en caracteres ASCII (49-57)
 function onlyNumberKey(evt) {    
-  // Only ASCII character in that range allowed
   var ASCIICode = (evt.which) ? evt.which : evt.keyCode
-  if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+  if (ASCIICode > 31 && (ASCIICode < 49 || ASCIICode > 57))
       return false;
   return true;
 }
-
 
 function numbers(){
   let numbers_matrix = '';
